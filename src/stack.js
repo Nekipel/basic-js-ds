067eccc39a08fec0260ca4893f9d31d090ddfb71
Nekipel <88710417+Nekipel@users.noreply.github.com>
2022-05-02 19:17:47 +0300

    feat:add new function remove-from-list

--- src/stack.js
+++ src/stack.js
@@ -13,20 +13,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  constructor() {this.array = [];}
+  pop() {return this.array.pop();}
+  push(element) {this.array.push(element);}
+  peek() {return this.array[this.array.length - 1];}
 }
 
 module.exports = {
